953fd325635248420f9d587bfda0d748c26bcf81
eminkartci <emin.kartci@ozu.edu.tr>
2021-03-26 10:36:19 +0300

    Create World / Turkey ^ Persons - Emin & Asya

---
+++ src/World.py
@@ -0,0 +1,15 @@
+
+# import classes
+from Community import  Community
+from Person import  Person
+
+
+# Create a Community
+TurkCommAsia = Community(1,"Turk","Turkey - Asia",340,"Muslim")
+TurkCommEuro = Community(1,"Turk","Turkey - Europe",240,"Muslim")
+
+# Create Some People
+P1 = Person(1,"Emin","Kartci","Male",21)
+P2 = Person(1,"Asya","Bostanoglu","Female",20)
+
+# Somehow we need to connect people and community
